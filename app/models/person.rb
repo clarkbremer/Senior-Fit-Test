c0085e085c7fee3c414569003aee176672a3e71b
Clark Bremer <cbremer@ackmanndickenson.com>
2015-05-29 16:10:02 -0500

    Get rid of all traces of `role`
    delegate stuff to user.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ActiveRecord::Base
   self.abstract_class = true
   has_one :user, as: :person, dependent: :destroy
   accepts_nested_attributes_for :user
+  delegate :name, to: :user
+  delegate :email, to: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  enum role: [:resident, :assessor, :admin]
-  #after_initialize :set_default_role, :if => :new_record?
   belongs_to :person, polymorphic: true
 
   # Include default devise modules. Others available are:
@@ -20,9 +18,4 @@ class User < ActiveRecord::Base
     self.person_type == "Resident" || self.person_type == "Assessor" || self.person_type == "Admin"
   end
 
-  private
-
-  def set_default_role
-    self.role = self.person_type.to_s.downcase
-  end
 end
