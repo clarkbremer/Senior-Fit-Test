e37117fb044197626b00e2aa2c4110120051a6e5
Clark Bremer <cbremer@ackmanndickenson.com>
2015-07-28 10:30:11 -0500

    Sendgrid Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
