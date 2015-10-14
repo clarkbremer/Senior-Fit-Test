82ef27d18267fb9c6337b667668799cebafd59be
Clark Bremer <cbremer@ackmanndickenson.com>
2015-10-14 10:48:52 -0500

    Don't delete residents when deleting a community.

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,6 @@
 class Community < ActiveRecord::Base
   has_and_belongs_to_many :assessors
-  has_many :residents, dependent: :destroy
+  has_many :residents
 
   validates_presence_of :name, :city, :state
 end
